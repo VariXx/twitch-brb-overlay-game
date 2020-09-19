814e79dda99c245c04b97126f34b7129a2dc89c9
VariXx <varixx@gmail.com>
2020-09-19 00:26:25 -0500

    added cors, added response text for testing

--- server/main.js
+++ server/main.js
@@ -1,13 +1,10 @@
 const express = require('express');
+const cors = require('cors');
 const bodyParser = require('body-parser');
 const { repsonse } = require('express');
 
 const app = express();
 const port = 16016; 
-
-app.get('/', (req, res) => {
-    res.send('go away');
-});
 
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false} ));
@@ -17,8 +14,13 @@ app.post('/users', (req, res) => {
     // update user info
 });
 
+app.get('/', (req, res) => {
+    res.send('go away');
+});
+
 app.get('/users', (req, res) => {
     // get user info
+    res.send('get user info');
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
